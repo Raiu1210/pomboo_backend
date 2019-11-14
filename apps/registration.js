9f71488ed8d3de67cd00f0bf022c6eb80a5700e3
Raiu1210 <baio1484@gmail.com>
2019-11-14 13:54:19 +0900

    simple register

--- apps/registration.js
+++ apps/registration.js
@@ -3,32 +3,33 @@
 const db = require('./connect_db')
 
 module.exports.registar_user = function(req, res) {
-    res.send({
-        message: 'this is registar'
-    })
-    console.log("this is registar")
-    
-    // console.log(req.body.user_name)
-    // console.log(req.body.email)
-    // console.log(req.body.password)
     let json = req.body
-
     const SQL_VAR = "email, password, user_name"
     const VALUES = "'" + json.email + "','" + json.password + "','" + json.user_name + "'"
 
-
     db.connect()
-    // let insert_sql = "INSERT INTO user_list (" + SQL_VAR + ") VALUES (" + VALUES + ");" 
+    // check user is already exists or not
     let sql = "select count(*) from user_list where email = '" + json.email + "';"
     db.query(sql, (err, rows, fields) => {
-        console.log(rows[0]['count(*)'])
-        if (err) throw err;    
-    });  
+        if (err) throw err;
+        if (rows[0]["count(*)"] < 0) {
+            // not existed
+            let insert_sql = "INSERT INTO user_list (" + SQL_VAR + ") VALUES (" + VALUES + ");" 
+            db.query(insert_sql, (err, rows, fields) => {
+                if (err) throw err;
+                res.send({
+                    message: 'registered'
+                })
+            })   
+        } else {
+            // existed
+            res.send({
+                message: 'this email address is already registered'
+            })
+        }
+    });    
     db.end();
-
-    console.log(sql)
-
-    
+    console.log(sql) 
 }
 
 console.log
