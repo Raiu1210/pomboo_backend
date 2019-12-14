b63d90eaa62786d009a6dcc1d673420e9e2be71a
Raiu1210 <baio1484@gmail.com>
2019-12-14 18:02:12 +0900

    added curl memo

--- apps/test.js
+++ apps/test.js
@@ -3,3 +3,13 @@ module.exports.test_func = function(req, res) {
         message: 'チンチン'
     })
 }
+
+// // register command
+// curl -X POST -H "Content-Type: application/json" -d '{"email":"baio1484@gmail.com", "password":"oppai", "user_name":"raiu220"}' localhost:3000/registration
+
+// // login command
+// curl -X POST -H "Content-Type: application/json" -d '{"email":"baio1484@gmail.com", "password":"oppai"}' localhost:3000/login
+
+// // update location command
+// curl -X POST -H "Content-Type: application/json" -d '{"email":"baio1484@gmail.com", "password":"oppai", "latitude":40.587290, "longitude":140.465143, "permission":1}' localhost:3000/update_location
+
