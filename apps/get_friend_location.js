21db7a4cf2cc1222422571dab3eaf2eeb627ff2e
Raiu1210 <baio1484@gmail.com>
2019-12-19 01:53:48 +0900

    calc time diff

--- apps/get_friend_location.js
+++ apps/get_friend_location.js
@@ -19,12 +19,32 @@ module.exports = async function(req, res) {
     // 0 : auth succeed
     if (auth_result["status"] == 0) {
         const user_id = auth_result.id
+        let friend_location = []
         
         const friend_list = await about_relation(user_id, 0, posted_data)
-        console.log(friend_list)
+
+        for(var i=0; i<friend_list.length; i++) {
+            const user_id = friend_list[i]["user_id"]
+            const level = friend_list[i]["level"]
+            const return_time = posted_data["return_time"]
+
+            // console.log({user_id})
+            // console.log({level})
+            // console.log({return_time})
+            get_recent_user_location(user_id, level, return_time)
+        }
 
         res.send({
             message: 'Your request result is here',
         })
     }
+}
+
+
+function get_recent_user_location(user_id, level, return_time) {
+    get_user_location_sql = "SELECT * FROM user_" + user_id + "_location " + 
+                            "WHERE permission <= " + level + " AND " +
+                                  "timestamp > NOW() - INTERVAL " + return_time + " HOUR;" 
+
+    console.log(get_user_location_sql)
 }
