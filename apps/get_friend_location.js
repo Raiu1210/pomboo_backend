e0c8797b0fd6a81ba06b87c8637fb21d5762ac0b
Raiu1210 <baio1484@gmail.com>
2019-12-19 16:43:16 +0900

    get friend location list function

--- apps/get_friend_location.js
+++ apps/get_friend_location.js
@@ -5,7 +5,7 @@ const crypto = require('crypto')
 // modules I wrote
 const user_auth = require('../my_modules/user_auth')
 const about_relation = require('../my_modules/about_relation')
-
+const db_config = require('../my_modules/db_config')
 
 module.exports = async function(req, res) { 
     // initialize variables with posted data
@@ -19,19 +19,19 @@ module.exports = async function(req, res) {
     // 0 : auth succeed
     if (auth_result["status"] == 0) {
         const user_id = auth_result.id
-        let friend_location = []
+        let friend_locations = []
         
         const friend_list = await about_relation(user_id, 0, posted_data)
 
         for(var i=0; i<friend_list.length; i++) {
             const user_id = friend_list[i]["user_id"]
             const level = friend_list[i]["level"]
-            const return_time = posted_data["return_time"]
+            const return_hour = posted_data["return_hour"]
 
             // console.log({user_id})
             // console.log({level})
             // console.log({return_time})
-            get_recent_user_location(user_id, level, return_time)
+            get_recent_user_location(user_id, level, return_hour)
         }
 
         res.send({
@@ -41,10 +41,20 @@ module.exports = async function(req, res) {
 }
 
 
-function get_recent_user_location(user_id, level, return_time) {
-    get_user_location_sql = "SELECT * FROM user_" + user_id + "_location " + 
-                            "WHERE permission <= " + level + " AND " +
-                                  "timestamp > NOW() - INTERVAL " + return_time + " HOUR;" 
+async function get_recent_user_location(user_id, level, return_hour) {
+    const get_user_location_sql = "SELECT * FROM user_" + user_id + "_location " + 
+                                  "WHERE permission <= " + level + " AND " +
+                                  "timestamp > NOW() - INTERVAL " + return_hour + " HOUR;" 
+
+
 
     console.log(get_user_location_sql)
+    // connect db
+    const conn = await mysql.createConnection(db_config);
+    try {
+        let [friend_location, fields] = await conn.query(get_user_location_sql);
+        console.log(friend_location)
+    } catch (err) {
+        throw err;
+    } 
 }
